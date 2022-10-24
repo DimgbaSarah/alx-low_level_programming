98812df69c2433e2387b7f4754145149d9ed0e1a
DimgbaSarah <sarahmdimgba@gmail.com>
2022-10-24 11:58:29 -0700

    task 0

---
+++ 0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * reset_to_98 - checks the updated value
+ * @n: number to be updated
+ * Return: Always 0
+ */
+void reset_to_98(int *n)
+{
+	int n;
+
+	*n = 98;
+	return (0);
+}
